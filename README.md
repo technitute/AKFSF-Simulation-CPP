25ee5b60f61c4e0b7495ca4c65c09aa1b3fdbe5a
Steven Dumble <steven.dumble@gmail.com>
2023-03-18 14:55:40 +1000

    Adding Capstone Answer

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Advanced Kalman Filtering and Sensor Fusion Simulation #
 
+Version 2.0
+
 Welcome to the Advanced Kalman Filtering and Sensor Fusion Simulation exercise project. In this project, you will be developing the source code for a number of different types of Kalman Filters which are used to estimate the navigation state of a 2D vehicle problem; such as that would be found on a self-driving car!
 
 
@@ -43,19 +45,18 @@ This project will use the Ubuntu 64 20.04.2.0 LTS VM C++ development environment
  ```
 ### Project Structure ###
 There are 6 main files of interest:
-* kalmanfilter_lkf_student.cpp
-* kalmanfilter_ekf_student.cpp
-* kalmanfilter_ukf_student.cpp
+* kalmanfilter_lkf.cpp
+* kalmanfilter_ekf.cpp
+* kalmanfilter_ukf.cpp
 * kalmanfilter_lkf_answer.cpp
 * kalmanfilter_ekf_answer.cpp
 * kalmanfilter_ukf_answer.cpp
+* kalmanfilter_ekf_capstone_answer.cpp
 
-To use one of the filter files, replace the ```kalmanfilter.cpp``` file with the one of interest.
-
-When carrying out the Linear Kalman Filter task, use the ```kalmanfilter_lkf_student.cpp``` as a base and develop all the exercise on top of each other. The file ```kalmanfilter_lkf_answer.cpp``` is a working version that you can use to compare your solution too. The same thing applies to the Extended Kalman Filter and Unscented Kalman Filter exercises with the corresponding files. You can use any of the files as a base for your capstone project.
+When carrying out the Linear Kalman Filter task, use the ```kalmanfilter_lkf.cpp``` as a base and develop all the exercise on top of each other. The file ```kalmanfilter_lkf_answer.cpp``` is a working version that you can use to compare your solution too. The same thing applies to the Extended Kalman Filter and Unscented Kalman Filter exercises with the corresponding files. You can use any of the files as a base for your capstone project.
 
 ## Simulation Operation ##
-The simulation can be run with different motion and sensor profiles to test the different scenarios and evaluate how the filter performs with different conditions. The different profiles can be activated pressing the number keys 1-9,0, to active the corresponding profile.
+The simulation can be run with different motion and sensor profiles to test the different scenarios and evaluate how the filter performs with different conditions. The different profiles can be activated pressing the number keys 1-9,0, to active the corresponding profile. The different types of Kalman Filters can be selected at run time using the keys: [l, e, u] for Linear, Extended and Unscented alman Filter versions.
 
 ### Motion Profiles ###
 * 1 - Constant Velocity + GPS + GYRO + Zero Initial Conditions
@@ -79,7 +80,7 @@ The tasks for this simulator can be broken down into 4 different areas:
 4. Capstone Project
 
 ### Linear Kalman Filter (LKF Exercise 1)
-Starting with the ```kalmanfilter_lkf_student.cpp``` and replace the ```kalmanfilter.cpp``` file. Initialize the filter on first prediction step and then implement the 2D Vehicle process model and Linear Kalman Filter Prediction steps. Test and verify good performance with profile 1.
+Starting with the ```kalmanfilter_lkf.cpp``` file. Initialize the filter on first prediction step and then implement the 2D Vehicle process model and Linear Kalman Filter Prediction steps. Test and verify good performance with profile 1.
 
 ### Linear Kalman Filter (LKF Exercise 2)
 Continuing on from Exercise 2, Implement the GPS update step using the Linear Kalman Filter Update equations. Test and verify good performance with profile 1. Play around with tunings and see results for profiles 2,3 and 4.
@@ -88,21 +89,19 @@ Continuing on from Exercise 2, Implement the GPS update step using the Linear Ka
 Continuing on from Exercise 3, Modify the filter to initialize the filter on first GPS measurement rather than prediction. Use the flag ```INIT_ON_FIRST_PREDICTION = false``` to enable this functionality. Test and verify good performance with profile 1 and 2.
 
 ### Extended Kalman Filter (EKF Exercise 1)
-Starting with the ```kalmanfilter_ekf_student.cpp``` and replace the ```kalmanfilter.cpp``` file. 
-Implement the 2D Vehicle process model and Extended Kalman Filter Prediction steps. Test and verify good performance with profiles 1-4.
+Starting with the ```kalmanfilter_ekf.cpp``` file. Implement the 2D Vehicle process model and Extended Kalman Filter Prediction steps. Test and verify good performance with profiles 1-4.
 
 ### Extended Kalman Filter (EKF Exercise 2)
 Continuing on from EKF Exercise 2, Implement the LIDAR update step using the Extended Kalman Filter Update equations. Test and verify good performance with profiles 1-8.
 
 ### Unscented Kalman Filter (UKF Exercise 1)
-Starting with the ```kalmanfilter_ukf_student.cpp``` and replace the ```kalmanfilter.cpp``` file. 
-Implement the 2D Vehicle Process model and Unscented Kalman Filter Prediction steps. Test and verify good performance with profiles 1-4.
+Starting with the ```kalmanfilter_ukf.cpp``` file. Implement the 2D Vehicle Process model and Unscented Kalman Filter Prediction steps. Test and verify good performance with profiles 1-4.
 
 ### Unscented Kalman Filter (UKF Exercise 2)
 Continuing on from UKF Exercise 2, Implement the LIDAR update step using the Unscented Kalman Filter Update equations. Test and verify good performance with profiles 1-8.
 
 ### Capstone Project
-Starting from any filter base replace the ```kalmanfilter.cpp``` file. Program a filter to provide the best estimation performance for profiles 9,0 (It should also work on any other profiles aswell).
+Starting from any filter base file. Program a filter to provide the best estimation performance for profiles 9,0 (It should also work on any other profiles aswell). You can compare to a solution that I have provided in ```kalmanfilter_ekf_capstone_answer.cpp```.
 
 ## Authors ##
 
---
+++ src/kalmanfilter_ekf_capstone_answer.cpp
@@ -0,0 +1,278 @@
+// ------------------------------------------------------------------------------- //
+// Advanced Kalman Filtering and Sensor Fusion Course - Extended Kalman Filter
+//
+// ####### CAPSTONE ANSWER FILE #######
+
+#include "kalmanfilter.h"
+#include "utils.h"
+
+// -------------------------------------------------- //
+// YOU CAN USE AND MODIFY THESE CONSTANTS HERE
+constexpr double ACCEL_STD = 1.0;
+constexpr double GYRO_STD = 0.01/180.0 * M_PI;
+constexpr double GYRO_BIAS_STD = 0.005/180.0 * M_PI; // Gyro Bias Process Model Noise (CAPSTONE)
+constexpr double INIT_VEL_STD = 10.0;
+constexpr double INIT_PSI_STD = 45.0/180.0 * M_PI;
+constexpr double GPS_POS_STD = 3.0;
+constexpr double LIDAR_RANGE_STD = 3.0;
+constexpr double LIDAR_THETA_STD = 0.02;
+// -------------------------------------------------- //
+
+void KalmanFilterEKF::handleLidarMeasurements(const std::vector<LidarMeasurement>& dataset, const BeaconMap& map)
+{
+    // Assume No Correlation between the Measurements and Update Sequentially
+    for(const auto& meas : dataset) {handleLidarMeasurement(meas, map);}
+}
+
+void KalmanFilterEKF::handleLidarMeasurement(LidarMeasurement meas, const BeaconMap& map)
+{
+    if (isInitialised())
+    {
+        VectorXd state = getState();
+        MatrixXd cov = getCovariance();
+
+        // Implement The Kalman Filter Update Step for the Lidar Measurements in the 
+        // section below.
+        // HINT: use the wrapAngle() function on angular values to always keep angle
+        // values within correct range, otherwise strange angle effects might be seen.
+        // HINT: You can use the constants: LIDAR_RANGE_STD, LIDAR_THETA_STD
+        // HINT: The mapped-matched beacon position can be accessed by the variables
+        // map_beacon.x and map_beacon.y
+        // ----------------------------------------------------------------------- //
+        // ENTER YOUR CODE HERE
+
+        BeaconData map_beacon = map.getBeaconWithId(meas.id); // Match Beacon with built in Data Association Id
+        if (meas.id != -1 && map_beacon.id != -1)
+        {           
+            // Measurement Vector
+            VectorXd z = Vector2d::Zero();
+            z << meas.range, meas.theta;
+
+            // Predicted Measurement Vector (Measurement Model)
+            VectorXd z_hat = Vector2d::Zero();
+            double delta_x = map_beacon.x - state[0];
+            double delta_y = map_beacon.y - state[1];
+            double zhat_range = sqrt(delta_x*delta_x + delta_y*delta_y);
+            double zhat_theta = wrapAngle(atan2(delta_y,delta_x) - state[2]);
+            z_hat << zhat_range, zhat_theta;
+
+            // Measurement Model Sensitivity Matrix
+            MatrixXd H = MatrixXd(2,5);
+            H << -delta_x/zhat_range,-delta_y/zhat_range,0,0,0,
+                  delta_y/zhat_range/zhat_range,-delta_x/zhat_range/zhat_range,-1,0,0; // Updated for Gyro Bias State (CAPSTONE)
+
+            // Generate Measurement Model Noise Covariance Matrix
+            MatrixXd R = Matrix2d::Zero();
+            R(0,0) = LIDAR_RANGE_STD*LIDAR_RANGE_STD;
+            R(1,1) = LIDAR_THETA_STD*LIDAR_THETA_STD;
+
+            VectorXd y = z - z_hat;
+            MatrixXd S = H * cov * H.transpose() + R;
+            MatrixXd K = cov*H.transpose()*S.inverse();
+
+            y(1) = wrapAngle(y(1)); // Wrap the Heading Innovation
+
+            state = state + K*y;
+            cov = (MatrixXd::Identity(5,5) - K*H) * cov;
+        }
+        // ----------------------------------------------------------------------- //
+
+        setState(state);
+        setCovariance(cov);
+    }
+    else
+    {
+        // Estimate Heading from Lidar Measurement (CAPSTONE)
+        BeaconData map_beacon = map.getBeaconWithId(meas.id); // Match Beacon with built in Data Association Id
+        if (meas.id != -1 && map_beacon.id != -1)
+        {
+            if (m_init_position_valid)
+            {
+                double delta_x = map_beacon.x - m_init_position_x;
+                double delta_y = map_beacon.y - m_init_position_y;
+                m_init_heading = wrapAngle(atan2(delta_y,delta_x) - meas.theta);
+                m_init_heading_valid = true;
+                std::cout << "INIT<HEADING> @ " << m_init_heading << std::endl;
+            }
+        }
+    }
+}
+
+void KalmanFilterEKF::predictionStep(GyroMeasurement gyro, double dt)
+{
+    if (isInitialised())
+    {
+        VectorXd state = getState();
+        MatrixXd cov = getCovariance();
+
+        // Implement The Kalman Filter Prediction Step for the system in the  
+        // section below.
+        // HINT: Assume the state vector has the form [PX, PY, PSI, V].
+        // HINT: Use the Gyroscope measurement as an input into the prediction step.
+        // HINT: You can use the constants: ACCEL_STD, GYRO_STD
+        // HINT: use the wrapAngle() function on angular values to always keep angle
+        // values within correct range, otherwise strange angle effects might be seen.
+        // ----------------------------------------------------------------------- //
+        // ENTER YOUR CODE HERE
+
+        double x = state(0);
+        double y = state(1);
+        double psi = state(2);
+        double V = state(3);
+        double bias = state(4);
+
+        // Update State
+        double x_new = x + dt * V * cos(psi);
+        double y_new = y + dt * V * sin(psi);
+        double psi_new = wrapAngle(psi + dt * (gyro.psi_dot - bias)); // Added Gyroscope Bias (CAPSTONE)
+        double V_new = V;
+        state << x_new,y_new,psi_new,V_new,bias_new; // Adding Gyro Bias State (CAPSTONE)
+
+        // Generate F Matrix
+        MatrixXd F = MatrixXd::Zero(5,5);
+        F << 1,0,-dt*V*sin(psi),dt*cos(psi),0, 
+             0,1,dt*V*cos(psi),dt*sin(psi),0,
+             0,0,1,0,-dt,
+             0,0,0,1,0,
+             0,0,0,0,1; // Updated for Gyro Bias State
+
+        // Generate Q Matrix
+        MatrixXd Q = MatrixXd::Zero(5,5);
+        Q(2,2) = dt*dt*GYRO_STD*GYRO_STD;
+        Q(3,3) = dt*dt*ACCEL_STD*ACCEL_STD;
+        Q(4,4) = dt*dt*GYRO_BIAS_STD*GYRO_BIAS_STD; // Adding Gyro Bias Process Model Noise (CAPSTONE)
+
+        cov = F * cov * F.transpose() + Q;
+
+        // ----------------------------------------------------------------------- //
+
+        setState(state);
+        setCovariance(cov);
+    }
+    else
+    {
+        // Assume Driving Straight or Stationary to Estimate Gyro Bias (CAPSTONE)
+        m_init_bias = gyro.psi_dot;
+        m_init_bias_valid = true;
+        std::cout << "INIT<BIAS> @ " << m_init_bias << std::endl;
+    }
+}
+
+void KalmanFilterEKF::handleGPSMeasurement(GPSMeasurement meas)
+{
+    // All this code is the same as the LKF as the measurement model is linear
+    // so the UKF update state would just produce the same result.
+    if(isInitialised())
+    {
+        VectorXd state = getState();
+        MatrixXd cov = getCovariance();
+
+        VectorXd z = Vector2d::Zero();
+        MatrixXd H = MatrixXd(2,5);
+        MatrixXd R = Matrix2d::Zero();
+
+        z << meas.x,meas.y;
+        H << 1,0,0,0,0,
+             0,1,0,0,0; // Updated for Gyro Bias State
+        R(0,0) = GPS_POS_STD*GPS_POS_STD;
+        R(1,1) = GPS_POS_STD*GPS_POS_STD;
+
+        VectorXd z_hat = H * state;
+        VectorXd y = z - z_hat;
+        MatrixXd S = H * cov * H.transpose() + R;
+        MatrixXd K = cov*H.transpose()*S.inverse();
+
+        // GPS Innovation Check (CAPSTONE)
+        VectorXd NIS = y.transpose()*S.inverse()*y;
+        if (NIS(0) < 5.99)
+        {
+            state = state + K*y;
+            cov = (MatrixXd::Identity(5,5) - K*H) * cov;
+        }
+        else
+        {
+            std::cout << "GPS NIS Failed! " << NIS(0) << std::endl;
+        }
+
+        setState(state);
+        setCovariance(cov);
+    }
+    else
+    {
+        // Set Initial Position (Only Update on First Measurement)
+        if (!m_init_position_valid)
+        {
+            m_init_position_x = meas.x;
+            m_init_position_y = meas.y;
+            m_init_position_valid = true;
+            std::cout << "INIT<POSITION> @ " << m_init_position_x <<"," << m_init_position_y  << std::endl;
+        }
+        
+        // Assume Zero Velocity
+        m_init_velocity = 0;
+        m_init_velocity_valid = true;
+        std::cout << "INIT<VELOCITY> @ " << m_init_velocity << std::endl;
+
+        // Estimate Heading from Sequential GPS Measurements (if not already from Lidar)
+        if (m_init_position_valid && !m_init_heading_valid)
+        {
+            // Estimate Delta from First Position
+            double delta_x = meas.x - m_init_position_x;
+            double delta_y = meas.y - m_init_position_y;
+            // Check If We have moved Enough
+            if ((delta_x*delta_x + delta_y*delta_y) > 3*GPS_POS_STD)
+            {
+                // Estimate Heading from Delta Position (Assuming Vehicle is Facing in the Direction it is moving)
+                m_init_heading = wrapAngle(atan2(delta_y,delta_x));
+                m_init_heading_valid = true;
+                std::cout << "INIT<HEADING> @ " << m_init_heading << std::endl;
+            }
+        }
+
+        if (m_init_position_valid &&
+            m_init_heading_valid &&
+            m_init_velocity_valid &&
+            m_init_bias_valid)
+        {
+            VectorXd state = VectorXd::Zero(5);
+            MatrixXd cov = MatrixXd::Zero(5,5);
+
+            state(0) = m_init_position_x;
+            state(1) = m_init_position_y;
+            state(2) = m_init_heading;
+            state(3) = m_init_velocity;
+            state(4) = m_init_bias;
+
+            cov(0,0) = GPS_POS_STD*GPS_POS_STD;
+            cov(1,1) = GPS_POS_STD*GPS_POS_STD;
+            cov(2,2) = LIDAR_THETA_STD*LIDAR_THETA_STD;
+            cov(3,3) = INIT_VEL_STD*INIT_VEL_STD;
+            cov(4,4) = GYRO_STD*GYRO_STD;
+
+            std::cout << "FILTER INIT" << time << std::endl;
+            
+            setState(state);
+            setCovariance(cov);
+        }
+    }     
+}
+
+Matrix2d KalmanFilterEKF::getVehicleStatePositionCovariance()
+{
+    Matrix2d pos_cov = Matrix2d::Zero();
+    MatrixXd cov = getCovariance();
+    if (isInitialised() && cov.size() != 0){pos_cov << cov(0,0), cov(0,1), cov(1,0), cov(1,1);}
+    return pos_cov;
+}
+
+VehicleState KalmanFilterEKF::getVehicleState()
+{
+    if (isInitialised())
+    {
+        VectorXd state = getState(); // STATE VECTOR [X,Y,PSI,V,...]
+        return VehicleState(state[0],state[1],state[2],state[3]);
+    }
+    return VehicleState();
+}
+
+void KalmanFilterEKF::predictionStep(double dt){}
